198dd3949b69d52f14dcb84fce62330adf7be568
Yoshinari Fujinuma <fujinumay@gmail.com>
2023-05-07 23:24:23 -0400

    add decoding part

--- examples/load_mpt.py
+++ examples/load_mpt.py
@@ -1,6 +1,24 @@
+import torch
 import transformers
 import wget
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+
+url = 'http://gutenberg.net.au/ebooks02/0200041.txt'  # the great gatsby full text
+filename = wget.download(url)
+print(filename)
+
+tmp_text = []
+with open(filename, errors="replace") as f:
+    for line in f:
+        tmp_text.append(line)
+        if len(tmp_text) > 750:
+            break
+text = "".join(tmp_text)
+text = text.replace("THE END", "EPILOGUE")
+text = text.replace("Project Gutenberg Australia", "")
+print(text[:-100])
+
+
 
 # non-triton ver.
 model_name = 'mosaicml/mpt-7b-storywriter'
@@ -8,37 +26,30 @@ config = transformers.AutoConfig.from_pretrained(
   model_name,
   trust_remote_code=True
 )
-config.update({"max_seq_len": 83968})
+#config.update({"max_seq_len": 83968})
+config.update({"max_seq_len": 8396})
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = transformers.AutoModelForCausalLM.from_pretrained(
   model_name,
   config=config,
+  torch_dtype=torch.bfloat16,
   trust_remote_code=True
 )
-#model.to(device='cuda:0')
-
-url = 'http://gutenberg.net.au/ebooks02/0200041.txt'  # the great gatsby full text
-filename = wget.download(url)
-with open(filename) as f:
-    text = f.readlines()
-text = text.replace("THE END", "EPILOGUE")
-text = text.replace("Project Gutenberg Australia", "")
-print(text[:-100])
+model.to(device='cuda:0')
 
 input_ids = tokenizer(text, return_tensors="pt").input_ids
-input_ids = input_ids.to(m.device)
-streamer = TextIteratorStreamer(tok, timeout=10.0, skip_prompt=True, skip_special_tokens=True)
+input_ids = input_ids.to(model.device)
+max_new_tokens = 120
+temperature = 0.1
 generate_kwargs = dict(
     input_ids=input_ids,
     max_new_tokens=max_new_tokens,
     temperature=temperature,
     do_sample=temperature > 0.0,
-    top_p=top_p,
-    top_k=top_k,
-    repetition_penalty=repetition_penalty,
-    streamer=streamer,
-    stopping_criteria=StoppingCriteriaList([stop]),
+    top_p=0.01,
+    top_k=1,
+    repetition_penalty=1.0,
 )
 
-output_text = m.generate(**generate_kwargs)
-print(output_text)
+output_text = model.generate(**generate_kwargs)
+print(tokenizer.decode(output_text[0], skip_special_tokens=True))
